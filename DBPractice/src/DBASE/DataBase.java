64f4f181d00b4b668c979ff0c4fd3d91d2522630
arelys alvarez <aalva456@ryloth.aul.fiu.edu>
2017-07-20 18:48:46 -0400

    part 1 done

--- DBPractice/src/DBASE/DataBase.java
+++ DBPractice/src/DBASE/DataBase.java
@@ -17,15 +17,22 @@ public class DataBase {
         Connection myconObj = null;     //connect to te database
         Statement mystatObj = null;     //execute queries on the database and get the results
         ResultSet myresObj = null;     //results will be stored here, present the data
-        String query = "";
+        String query = "Select * from AALVA456.SCHOOLS";
         String url;
         String user;
         String password;
         
         try{
-           myconObj = DriverManager.getConnection("jdbc:derby://localhost1527/Information[aalva456 on AALVA456]", "aalva456", "123456");
+           myconObj = DriverManager.getConnection("jdbc:derby://localhost1527/Information", "aalva456", "123456");
            mystatObj = myconObj.createStatement();
-           myresObj = mystatObj.executeQuery("");
+           myresObj = mystatObj.executeQuery(query);
+           while(myresObj.next()){
+               int number = myresObj.getInt("NUMBER");
+               String name = myresObj.getString("NAME");
+               Date date = myresObj.getDate("DATE");
+               String phonenumb = myresObj.getString("PHONENUMBER");
+               System.out.println(number + name + date + phonenumb);
+           }
         }
         catch(SQLException e){
             e.printStackTrace();
